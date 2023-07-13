4ff328861d88d33db863467c1055e2b843032e15
ɴᴏxɪᴅᴇᴜꜱ <92509001+Yahya-rabii@users.noreply.github.com>
2023-07-13 12:20:46 +0100

    edit connection string and register view

--- Migrations/20230713111536_Init.Designer.cs
+++ Migrations/20230713111536_Init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GAP.Migrations
 {
     [DbContext(typeof(GAPContext))]
-    [Migration("20230713105037_Init")]
+    [Migration("20230713111536_Init")]
     partial class Init
     {
         /// <inheritdoc />
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using GAP.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<GAPContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GAPContext") ?? throw new InvalidOperationException("Connection string 'GAPContext' not found.")));
@@ -8,7 +11,32 @@ builder.Services.AddDbContext<GAPContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddCookie(options =>
+{
+
+    options.LoginPath = "/Users/Login";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+    options.SlidingExpiration = true;
+});
+
+
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("isrespachat", policy => policy.RequireClaim(ClaimTypes.Role, "respachat"));
+    options.AddPolicy("isrecepachat", policy => policy.RequireClaim(ClaimTypes.Role, "recepachat"));
+    options.AddPolicy("isrespfin", policy => policy.RequireClaim(ClaimTypes.Role, "respfin"));
+    options.AddPolicy("isrespqual", policy => policy.RequireClaim(ClaimTypes.Role, "respqual"));
+});
+
+
+builder.Services.AddSession();
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
